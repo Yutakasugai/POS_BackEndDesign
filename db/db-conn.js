9933806ad613f60988709495f6f259c7a53ad770
yuta <yuta>
2022-12-03 01:37:42 -0800

    I changed it again.

--- db/db-conn.js
+++ db/db-conn.js
@@ -4,23 +4,9 @@ const mysql = require("mysql");
 // dotenv.config({path : "./.env"}); 
 
 // Create connection to database 
-// function db_conn(){
-
-//     const db = mysql.createConnection({
-//         host: 'us-cdbr-east-06.cleardb.net',
-//         user: 'b2a454764cb73f', 
-//         password: '6eb564fe',
-//         database: 'heroku_719ef538f12f1bb'
-//     });
-
-//     return db; 
-// }
-
-
-// Create connection as a pool 
 function db_conn(){
 
-    const db = mysql.createPool({
+    const db = mysql.createConnection({
         host: 'us-cdbr-east-06.cleardb.net',
         user: 'b2a454764cb73f', 
         password: '6eb564fe',
@@ -29,5 +15,19 @@ function db_conn(){
 
     return db; 
 }
+
+
+// Create connection as a pool 
+// function db_conn(){
+
+//     const db = mysql.createPool({
+//         host: 'us-cdbr-east-06.cleardb.net',
+//         user: 'b2a454764cb73f', 
+//         password: '6eb564fe',
+//         database: 'heroku_719ef538f12f1bb'
+//     });
+
+//     return db; 
+// }
 
 exports.db_conn = db_conn();
